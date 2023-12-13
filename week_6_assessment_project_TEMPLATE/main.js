08b3a27aec773856d85990c66c193b2810e97866
LB45000 <144392042+LB45000@users.noreply.github.com>
2023-12-12 18:11:11 -0800

    Update main.js

--- week_6_assessment_project_TEMPLATE/main.js
+++ week_6_assessment_project_TEMPLATE/main.js
@@ -1,12 +1,22 @@
-console.log("connected to main.js")
+console.log("connected to main.js");
 
 // array used to track all the food categories
 // TASK 1
-    // Add more categories to the CATEGORY array
-    // Test that each works with the fetch request
+// Add more categories to the CATEGORY array
+// Test that each works with the fetch request
 const CATEGORY = [
   "Starter",
-]
+  "Beef",
+  "Chicken",
+  "Lamb",
+  "Pasta",
+  "Pork",
+  "Seafood",
+  "Vegetarian",
+  "Vegan",
+  "Side",
+  "Dessert"
+];
 
 // select element that will store our category options
 let categories = document.getElementById("category");
@@ -37,18 +47,23 @@ function getFetch() {
         container.classList.add("mealContainer");
         mealsList.appendChild(container);
 
-        // use the example above to complete TASK 2
         // TASK 2
-        // 1) add a header to the container
-        // 2) add an image to the container UNDER the header
-          // ex: "Broccoli & Stilton soup", followed by a picture of
-          // broccoli & stilton soup
-        // ... add code here
+        // 1) Add a header to the container
+        let header = document.createElement("h2");
+        header.textContent = data.meals[i].strMeal;
+        container.appendChild(header);
+
+        // 2) Add an image to the container UNDER the header
+        let image = document.createElement("img");
+        image.src = data.meals[i].strMealThumb;
+        image.alt = data.meals[i].strMeal; // Set alt text for accessibility
+        image.style.width = "100%"; // Set appropriate width to avoid stretching/warping
+        container.appendChild(image);
       }
 
       return data;
     })
     .catch(err => {
-      console.log(`error ${err}`)
+      console.log(`error ${err}`);
     });
 }
